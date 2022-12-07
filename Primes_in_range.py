fa265c4fed85537f7f9fff0a1d6146226bc22fb1
bhavanishankar <98006569+bhavanishankar4@users.noreply.github.com>
2022-12-07 15:33:45 +0530

    import math def prime(x):     if x==0 or x==1:         return False     else:         for i in range(2,int(math.sqrt(x))+1):             if x%i==0:                 return False     return True a=int(input()) b=int(input()) c=0 for i in range(a,b+1):     if prime(i)==True:         c+=1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,16 @@
+import math
+def prime(x):
+    if x==0 or x==1:
+        return False
+    else:
+        for i in range(2,int(math.sqrt(x))+1):
+            if x%i==0:
+                return False
+    return True
+a=int(input())
+b=int(input())
+c=0
+for i in range(a,b+1):
+    if prime(i)==True:
+        c+=1
+print(c)
