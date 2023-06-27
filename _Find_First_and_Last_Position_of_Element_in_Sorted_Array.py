35c51cb2cdf893805f755cac6d41c90a11df0b0d
bhavanishankar <98006569+bhavanishankar4@users.noreply.github.com>
2023-06-27 15:49:58 +0530

    n=int(input()) l=list(map(int,input().split())) a=int(input()) b=[] c=[] for i in range(len(l)):     if l[i]==a:         b.append(i) if len(b)>0:     print(b[0],b[-1]) else:     print(-1,-1)

---
+++ _Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=list(map(int,input().split()))
+a=int(input())
+b=[]
+c=[]
+for i in range(len(l)):
+    if l[i]==a:
+        b.append(i)
+if len(b)>0:
+    print(b[0],b[-1])
+else:
+    print(-1,-1)
