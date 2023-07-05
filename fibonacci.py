82b26adce9ea6bb078f2d6d579c30813e4b683e4
bhavanishankar <98006569+bhavanishankar4@users.noreply.github.com>
2023-07-05 15:29:57 +0530

    n = int(input()) a = 0 b = 1 sum = a + b count = 1
    while (count <= n):
    	count += 1
    	print(a, end=" ")
    	a = b
    	b = sum
    	sum = a + b

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,12 @@
-
-    
-    
-n=int(input())
+n = int(input())
 a = 0
 b = 1
-sum = 0
+sum = a + b
 count = 1
 
-while(count <= n):
-  print(sum, end = " ")
-  count += 1
-  a = b
-  b = sum
-  sum = a + b
+while (count <= n):
+	count += 1
+	print(a, end=" ")
+	a = b
+	b = sum
+	sum = a + b
